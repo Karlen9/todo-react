22ab85b85f74b783b350a3c3169c8be56a4820b3
Karlen Misakyan <misakyankarlen@gmail.com>
2021-03-25 15:55:31 +0300

    interseptor first add

--- src/ToDoList.js
+++ src/ToDoList.js
@@ -280,6 +280,14 @@ export default function ToDoList() {
     } 
   };
 
+
+  // axios.interceptors.request.use(function (config) {
+  //   // Do something before request is sent
+  // }, function (error) {
+  //   // Do something with request error
+  //   setErrMessage(Promise.reject(error));
+  // });
+
     //localStorage
   
   // const saveLocalTodos = () => {
