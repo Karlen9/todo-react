3dbc073d809bd51ef5b0a36dd0a0d9c574a69c3b
Karlen Misakyan <misakyankarlen@gmail.com>
2021-04-14 14:19:53 +0300

    Header added

--- src/App.js
+++ src/App.js
@@ -6,11 +6,18 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 export default function App() {
   return (
     <div className="App">
+      <h1>ToDo</h1>
       <Router>
         <Switch>
-          <Route path="/todos" component={ToDoList} />
-          <Route path="/login" component={SignIn} />
-          <Route path="/register" component={SignUp} />
+          <Route path="/todos">
+            <ToDoList />
+          </Route>
+          <Route path="/login">
+            <SignIn />
+          </Route>
+          <Route path="/register">
+            <SignUp />
+          </Route>
         </Switch>
       </Router>
     </div>
